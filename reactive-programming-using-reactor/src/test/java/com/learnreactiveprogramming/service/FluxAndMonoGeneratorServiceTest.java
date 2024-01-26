d520c355019dc269c8ea5a2e49f66d1c4d00f63a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-27 00:57:00 +0300

    Create unit tests for Flux and Mono

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,29 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService generatorService = new FluxAndMonoGeneratorService();
 
+    @Test
+    void namesFluxTest() {
+        Flux<String> namesFlux = generatorService.namesFlux();
+        StepVerifier.create(namesFlux)
+                // .expectNextCount(3)
+                .expectNext("Alex", "Ben", "Chloe")
+                // .expectNext("Alex")
+                // .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMonoTest() {
+        Mono<String> namesMono = generatorService.namesMono();
+        StepVerifier.create(namesMono)
+                .expectNext("Alex")
+                .verifyComplete();
+    }
 }
