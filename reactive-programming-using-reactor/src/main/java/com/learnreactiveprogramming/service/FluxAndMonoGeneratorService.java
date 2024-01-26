faf3f7d51dc8a6cc08959db4861131be8bb95947
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-26 22:48:40 +0300

    Create the first Flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,17 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService generatorService = new FluxAndMonoGeneratorService();
+        generatorService.namesFlux()
+                .subscribe(name -> System.out.printf("Name is: %s\n", name));
+    }
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
+    }
 }
