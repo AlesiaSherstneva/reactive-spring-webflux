ea3d1aabdf44175ca1459d4a588f361c07b3d7c2
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-02-23 21:19:14 +0300

    Add GET method to retrieve all reviews

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -7,12 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class ReviewHandler {
     private final ReviewReactiveRepository reviewReactiveRepository;
+
+    public Mono<ServerResponse> getReviews() {
+        Flux<Review> reviewsFlux = reviewReactiveRepository.findAll();
+        return ServerResponse.ok().body(reviewsFlux, Review.class);
+    }
 
     public Mono<ServerResponse> addReview(ServerRequest request) {
         return request.bodyToMono(Review.class)
