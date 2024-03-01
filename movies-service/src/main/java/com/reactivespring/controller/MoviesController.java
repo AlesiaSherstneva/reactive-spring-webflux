77c528d39997ed778bef48a56d4504ff17d98017
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-01 21:01:18 +0300

    Create movies controller

---
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -0,0 +1,10 @@
+package com.reactivespring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vi/movies")
+public class MoviesController {
+
+}
