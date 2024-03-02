560bb59af8bc1185f59b16837cf970ec8ebb0692
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-02 17:03:31 +0300

    Add functionality for retrieving data from movies review service

--- movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
@@ -1,11 +1,29 @@
 package com.reactivespring.client;
 
+import com.reactivespring.domain.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 
 @Component
 @RequiredArgsConstructor
 public class ReviewRestClient {
     private final WebClient webClient;
+
+    @Value("${restClient.reviewsUrl}")
+    private String reviewsUrl;
+
+    public Flux<Review> retrieveReviews(String movieId) {
+        String url = UriComponentsBuilder.fromHttpUrl(reviewsUrl)
+                .queryParam("movieInfoId", movieId)
+                .buildAndExpand().toUriString();
+
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToFlux(Review.class);
+    }
 }
