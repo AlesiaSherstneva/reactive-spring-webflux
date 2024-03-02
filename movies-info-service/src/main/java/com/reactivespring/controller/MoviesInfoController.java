e967122fc4cef33d8d61032d046d0ce788e428f3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-02 16:41:02 +0300

    Add functionality for retrieving data from movies info service

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -4,6 +4,7 @@ import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MoviesInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -2,6 +2,7 @@ package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,7 +12,14 @@ import reactor.core.publisher.Mono;
 public class MoviesInfoRestClient {
     private final WebClient webClient;
 
-    public Mono<MovieInfo> retrieveMovieInfo(String movieId) {
+    @Value("${restClient.moviesInfoUrl}")
+    private String moviesInfoUrl;
 
+    public Mono<MovieInfo> retrieveMovieInfo(String movieId) {
+        String url = moviesInfoUrl.concat("/{id}");
+        return webClient.get()
+                .uri(url, movieId)
+                .retrieve()
+                .bodyToMono(MovieInfo.class);
     }
 }
