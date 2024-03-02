2dc3560a2ef91f8548ab247f55677925471d7998
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-02 16:52:40 +0300

    Create class to retrieve movie reviews

---
+++ movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
@@ -0,0 +1,11 @@
+package com.reactivespring.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewRestClient {
+    private final WebClient webClient;
+}
