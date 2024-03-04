1098a4919b07d30fcd244fa3d34f4230de08236f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-04 22:17:03 +0300

    Handle exceptions on movie service layer

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,14 +1,19 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MoviesInfoRestClient {
     private final WebClient webClient;
 
@@ -20,6 +25,28 @@ public class MoviesInfoRestClient {
         return webClient.get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError,
+                        clientResponse -> {
+                            log.info("Status code is: {}", clientResponse.statusCode().value());
+                            if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                                return Mono.error(new MoviesInfoClientException(
+                                        "There is no movie info with id " + movieId,
+                                        clientResponse.statusCode().value()));
+                            }
+                            return clientResponse.bodyToMono(String.class)
+                                    .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                            responseMessage,
+                                            clientResponse.statusCode().value()
+                                    )));
+                        })
+                .onStatus(HttpStatus::is5xxServerError,
+                        clientResponse -> {
+                            log.info("Status code is: {}", clientResponse.statusCode().value());
+                            return clientResponse.bodyToMono(String.class)
+                                    .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException(
+                                            String.format("Server Exception in movies info service: %s", responseMessage)
+                                    )));
+                        })
                 .bodyToMono(MovieInfo.class);
     }
 }
--- movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
@@ -1,15 +1,21 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewRestClient {
     private final WebClient webClient;
 
@@ -24,6 +30,24 @@ public class ReviewRestClient {
         return webClient.get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError,
+                        clientResponse -> {
+                            log.info("Status code is: {}", clientResponse.statusCode().value());
+                            if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                                return Mono.empty();
+                            }
+                            return clientResponse.bodyToMono(String.class)
+                                    .flatMap(responseMessage -> Mono.error(new ReviewsClientException(
+                                            responseMessage)));
+                        })
+                .onStatus(HttpStatus::is5xxServerError,
+                        clientResponse -> {
+                            log.info("Status code is: {}", clientResponse.statusCode().value());
+                            return clientResponse.bodyToMono(String.class)
+                                    .flatMap(responseMessage -> Mono.error(new ReviewsServerException(
+                                            String.format("Server Exception in movies reviews service: %s", responseMessage)
+                                    )));
+                        })
                 .bodyToFlux(Review.class);
     }
 }
