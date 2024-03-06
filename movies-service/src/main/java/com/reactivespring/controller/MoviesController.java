fcbf0f36b084766c4b4f8864020eb52df80a3b89
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-07 01:29:15 +0300

    Change 'vi' to 'v1' in controller url

--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -14,7 +14,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vi/movies")
+@RequestMapping("/v1/movies")
 @RequiredArgsConstructor
 public class MoviesController {
     private final MoviesInfoRestClient moviesInfoRestClient;
